5b7642d836f563493219a1100d1483973031051b
Zakhar Agafonov <z.agafonov@smartway.today>
2025-01-11 17:41:07 +0300

    init

---
+++ responseWrapper/errorWrap.go
@@ -0,0 +1,27 @@
+package responseWrapper
+
+import (
+	"encoding/json"
+	"net/http"
+	"utils/context"
+)
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(ctx context.MyContext, w http.ResponseWriter, message string, statusCode int) {
+	ctx.Logger.Error(message)
+
+	errRes := ErrorResponse{Message: message}
+
+	jsonErrRes, err := json.Marshal(errRes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set(ContentType, ApplicationJSON)
+	w.WriteHeader(statusCode)
+	w.Write(jsonErrRes)
+}
